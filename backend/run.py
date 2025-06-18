046280fd54462cfb1e0d86b7a546bacddbb7724a
AayushPurivsKartik <aayushpuri683@gmail.com>
2025-06-18 12:16:40 +0530

    Changes in file

--- backend/run.py
+++ backend/run.py
@@ -3,11 +3,6 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="huggingface_hu
 
 from app import create_app, db
 from flask_migrate import Migrate
-import nltk
-import spacy.cli
-spacy.cli.download("en_core_web_sm")
-nltk.download('wordnet')
-nltk.download('omw-1.4')
 app = create_app()
 migrate = Migrate(app, db)
 
